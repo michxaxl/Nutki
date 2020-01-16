0d5d45f4e68687c56bf4f483a709a89e7b46f701
michxaxl <michxaxl@gmail.com>
2020-01-16 18:00:40 +0100

    added MVC and changed font

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,14 @@
 public class Controller {
 
+    private Model model;
+    private App view;
+
+    private Thread thread;
+
+    public Controller(Model m, App v) {
+        model = m;
+        view = v;
+    }
+
+
 }
