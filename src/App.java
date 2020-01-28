3f089a26df575ed253ae3dcd7bdcc52726cf3682
michxaxl <michxaxl@gmail.com>
2020-01-29 00:50:49 +0100

    added interaction to buttons and developed the keyboard class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
 import sun.applet.Main;
 import javax.sound.sampled.*;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class App {
@@ -12,27 +15,34 @@ public class App {
     private JButton checkButton;
     private JButton nextButton;
     private JLabel noteLabel;
-    private JButton buttonC;
-    private JButton buttonD;
-    private JButton buttonE;
-    private JButton buttonF;
-    private JButton buttonG;
-    private JButton buttonA;
-    private JButton buttonH;
-    private JButton buttonCis;
-    private JButton buttonDis;
-    private JButton buttonFis;
-    private JButton buttonGis;
-    private JButton buttonB;
+    private JButton buttonC, buttonD, buttonE, buttonF, buttonG, buttonA, buttonH;
+    private JButton buttonCis, buttonDis, buttonFis, buttonGis, buttonB;
+    private JButton[] buttonsArray = {buttonC, buttonD, buttonE, buttonF, buttonG, buttonA, buttonH, buttonCis, buttonDis, buttonFis, buttonGis, buttonB};
     private JLabel pointsLabel;
-    private Keyboard keyboard;
+    private JPanel keyboardPanel;
     private static String pathToNotes;
     private static String[] notesArray = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "B", "H"};
     private static String drawnNote = "";
+    private int points = 0;
+    private Keyboard keyboard;
+
 
 
     public App() throws IOException{
-//        keyboard.loadImage();
+        keyboard = new Keyboard(buttonsArray);
+
+        buttonC.setName("C");
+        buttonD.setName("D");
+        buttonE.setName("E");
+        buttonF.setName("F");
+        buttonG.setName("G");
+        buttonA.setName("A");
+        buttonH.setName("H");
+        buttonCis.setName("C#");
+        buttonDis.setName("D#");
+        buttonFis.setName("F#");
+        buttonGis.setName("G#");
+        buttonB.setName("B");
 
         pathToNotes = "/Notes/";
         nextButton.addActionListener(new ActionListener() { // Nastepny
@@ -46,10 +56,20 @@ public class App {
         checkButton.addActionListener(new ActionListener() { // Sprawdz
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(noteField.getText().equals(drawnNote)){
+                String typed = noteField.getText().toUpperCase();
+                /* Jezeli wpisany dzwiek jest taki sam jak wylosowany */
+                if(typed.equals(drawnNote)){
                     good();
                 }else{
                     bad();
+//                    for(String note : notesArray){
+//                        if(typed.equals(note))
+//                            bad();
+//                        else
+//                            bad();
+////                            noteLabel.setText("Nie ma takiego dźwięku");
+//                    }
+
                 }
             }
         });
@@ -80,16 +100,46 @@ public class App {
     }
 
     public void good() {
-        noteLabel.setText("Dobrze!");
-        noteField.setText("");
+        keyboard.addPoint(drawnNote);
 
-        drawSound();
+        pointsLabel.setForeground(new Color(122, 226, 58));
+        noteLabel.setText("Dobrze!");
+        noteField.setText(""); // Wyczyszczenie pola tekstowego
+        points+=2;
+        updatePoints();
+        drawSound(); // losowanie kolejnego dźwięku
     }
 
     public void bad() {
+        pointsLabel.setForeground(new Color(255, 103, 73));
         noteLabel.setText("Niestety, był to dźwięk: "+drawnNote);
         noteField.setText("");
+        points--;
+        updatePoints();
         drawSound();
+    }
+
+    public void updatePoints(){
+        if(points<0){ points=0;}
+
+
+//
+
+        Font labelFont = pointsLabel.getFont();
+        pointsLabel.setText("PUNKTY: "+points);
+
+        pointsLabel.setFont(labelFont.deriveFont(labelFont.getStyle() | Font.BOLD)); //Ustawienie pogrubionej czcionki
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(1300);
+                    pointsLabel.setFont(labelFont.deriveFont(labelFont.getStyle() & ~Font.BOLD)); // Wyłączenie pogrubionej czcionki po 1300 milisekundach
+                    pointsLabel.setForeground(Color.BLACK);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
     }
 
     public static void main(String[] args) throws IOException {
--- src/Keyboard.java
+++ src/Keyboard.java
@@ -1,35 +1,99 @@
+import sun.applet.Main;
+
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Keyboard extends JComponent {
-    private BufferedImage image;
+public class Keyboard {
 
-    public void loadImage() throws IOException {
+    private JButton[] buttonsArray;
+    private Map<String, Integer> pointsForNote = new HashMap<>();
 
+    public Keyboard(JButton[] butArray) {
+        this.buttonsArray = butArray;
+        pointsForNote.put("C", 0);
+        pointsForNote.put("C#", 0);
+        pointsForNote.put("D", 0);
+        pointsForNote.put("D#", 0);
+        pointsForNote.put("E", 0);
+        pointsForNote.put("F", 0);
+        pointsForNote.put("F#", 0);
+        pointsForNote.put("G", 0);
+        pointsForNote.put("G#", 0);
+        pointsForNote.put("A", 0);
+        pointsForNote.put("B", 0);
+        pointsForNote.put("H", 0);
+    }
+
+    /* Pobiera ilosc punktow dla danego dzwieku */
+    public Integer getPoints(String note) {
+        return pointsForNote.get(note);
+    }
+
+    /* Ustawia ilosc punktow points dla dzwieku note*/
+    public void setPoints(String note, Integer points) {
+        pointsForNote.put(note, points);
+    }
+
+    /* Dodaje 1 punkt */
+    public void addPoint(String note)
+    {
         try {
-            image = ImageIO.read(getClass().getResource("/img/keyboard.jpg"));
-        } catch (IOException e) {
-            System.err.println("Blad odczytu obrazka");
-            e.printStackTrace();
+            Integer actualPoints = pointsForNote.get(note);
+            pointsForNote.put(note, ++actualPoints);
+            for(JButton but : buttonsArray){ // Pętla po buttonach
+                if(note.equals(but.getName())){ // Jeżeli wylosowany dźwięk jest taki sam jak nazwa buttona
+                    if(getPoints(note)==1){ // Jezeli 3 razy odgadnieto dzwiek
+                        but.setText(note); // Pokazanie go na klawiaturze
+                        but.addActionListener(new ActionListener() { //Dodaje listener do buttona zeby mogl wydawac dzwiek
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                playSound(note);
+                            }
+                        });
+                    }
+
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
         }
-        this.setBorder(BorderFactory.createEmptyBorder());
+    }
 
+//    void button(JButton button) {
+//        button.addActionListener(new ActionListener() { // Nastepny
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//
+//            }
+//        }
+//    }
+
+    public void playSound(String note) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+//                    Thread.sleep(1500);
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            Main.class.getResourceAsStream("/notes/" + note + ".wav"));
+                    clip.open(inputStream);
+                    clip.start();
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
     }
 
 
-    @Override
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D)g;
-        g2d.drawImage(image, 0, 0, this);
-    }
 
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(image.getWidth(),image.getHeight());
-    }
 }
